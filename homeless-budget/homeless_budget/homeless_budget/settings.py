86579d195e154747642be64d474a5eca4fe295e3
jos-h20 <josh_overly@yahoo.com>
2016-12-07 20:43:23 -0800

    change script to add password

--- homeless-budget/homeless_budget/homeless_budget/settings.py
+++ homeless-budget/homeless_budget/homeless_budget/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'homeless_budget.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'homeless',
+       'NAME': 'vagrant',
        'USER': 'vagrant',
        'PASSWORD': 'vagrant',
        'HOST': 'localhost',
